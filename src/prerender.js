79b17ad3fadc3a6bd4d0677403fe1848de7c0176
Kevin Jannis <=>
2013-10-12 11:43:00 +0200

    Add prerender and remove link

--- src/prerender.js
+++ src/prerender.js
@@ -10,10 +10,31 @@
     this.options = {
       name: defaults.name,
       timeout: opts.timeout || defaults.timeout,
-      selector: opts.selector || defaults.selector
+      selector: opts.selector || defaults.selector,
+      linkId: 'prerender'
     };
-
-    
   };
 
+  PreRender.prototype.prerender = function(href) {
+    this.remove();
+
+    var link = document.createElement('link');
+    link.setAttribute('id', this.linkId);
+    link.setAttribute('rel', 'prerender');
+    link.setAttribute('href', href);
+
+    var head = document.getElementsByTagName('head')[0];
+    if (head) {
+      head.appendChild(link);
+    }
+  };
+
+  PreRender.prototype.remove = function() {
+    var link = document.findElementById(this.linkId);
+    if (link && link.parentNode) {
+      link.parentNode.removeChild(link);
+    }
+  };
+
+  
 })();
